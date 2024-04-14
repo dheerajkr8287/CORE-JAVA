bdffd21b1fd581a5ac84a5117b55e1e0a9e655af
Dheeraj kumar <119065267+dheerajkr8287@users.noreply.github.com>
2024-04-15 00:42:24 +0530

    Update thread_method.java

--- MultiThreading/thread_method.java
+++ MultiThreading/thread_method.java
@@ -48,3 +48,55 @@ public static void main(String[] args) {
     t2.start();
 }
 }
+
+
+
+
+
+
+/*
+Interrupt() method :
+A thread in a sleeping or waiting state can be interrupted by another thread with the help of the interrupt() method of the Thread class.
+The interrupt() method throws InterruptedException.
+The interrupt() method will not throw the InterruptedException if the thread is not in the sleeping/blocked state, but the interrupt flag will be changed to true.
+Syntax :
+Public void interrupt()
+*/
+
+
+
+/*class CWH1 extends Thread{
+    public void run(){
+        try {
+            for (int i=0;i<5;i++){
+                System.out.println("Child Thread");
+                Thread.sleep(4000); //Child thread is put to sleep for 4000ms. As soon as child thread goes to sleep main thread interrupts it. And, InterruptedException is generated which is handled by the catch block. 
+
+            }
+        }
+        catch (InterruptedException e)
+        {
+            System.out.println("Child Thread Interrupted");
+        }
+	System.out.println("Thread is running");
+
+    }
+}
+
+public class thread_method extends Thread{
+    public static void main(String[] args) {
+         CWH1 t= new CWH1();
+         t.start();
+         t.interrupt();
+        System.out.println("Main Thread");
+
+    }
+}
+*/
+
+/*
+In the above code, the for loop runs for the first time, but the child thread is put to sleep after that. 
+So, the main() method interrupts the child thread, and InterruptedException is generated.
+Here, the child thread comes out of the sleeping state, but it does not stop working.
+*/
+
